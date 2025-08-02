9b7df03b4e547f430e5aac2dc85be31df3baac12
root <root@localhost.localdomain>
2025-08-02 13:14:39 -0500

    imagenes

--- src/app/components/marketing/marketing.component.ts
+++ src/app/components/marketing/marketing.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-
+import { GlobalService } from '../../services/global.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-marketing',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './marketing.component.html',
   styleUrl: './marketing.component.css'
 })
 export class MarketingComponent {
-
+constructor(public global: GlobalService) {}
 }
